6d9450df79bbdfd730caf34367cac5f7f20a4175
Sandra <sandrajerotich1@gmail.com>
2024-07-14 04:39:03 +0300

    Activity Indicator

--- screens/DirectoryScreen.js
+++ screens/DirectoryScreen.js
@@ -1,21 +1,34 @@
-
-import { FlatList } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
 import { Tile } from 'react-native-elements';
 import { useSelector } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from '../components/LoadingComponent';
+
 const DirectoryScreen = ({ navigation }) => {
     const campsites = useSelector((state) => state.campsites);
+
+    if (campsites.isLoading) {
+        return <Loading />;
+    }
+    if (campsites.errMess) {
+        return (
+            <View>
+                <Text>{campsites.errMess}</Text>
+            </View>
+        );
+    }
+
     const renderDirectoryItem = ({ item: campsite }) => {
         return (
             <Tile
-            title={campsite.name}
-            caption={campsite.description}
-            featured
-            onPress={() =>
-                navigation.navigate('CampsiteInfo', { campsite })
-            }
-            imageSrc={{ uri: baseUrl + campsite.image }}
-        />
+                title={campsite.name}
+                caption={campsite.description}
+                featured
+                onPress={() =>
+                    navigation.navigate('CampsiteInfo', { campsite })
+                }
+                imageSrc={{ uri: baseUrl + campsite.image }}
+            />
         );
     };
     return (
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,12 +2,25 @@ import { ScrollView, Text, View } from 'react-native';
 import { Card } from 'react-native-elements';
 import { useSelector } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from '../components/LoadingComponent';
 
-const FeaturedItem = ({ item }) => {
+const FeaturedItem = (props) => {
+    const { item } = props;
+
+    if (props.isLoading) {
+        return <Loading />;
+    }
+    if (props.errMess) {
+        return (
+            <View>
+                <Text>{props.errMess}</Text>
+            </View>
+        );
+    }
     if (item) {
         return (
             <Card containerStyle={{ padding: 0 }}>
-               <Card.Image source={{ uri: baseUrl + item.image }}>
+                <Card.Image source={{ uri: baseUrl + item.image }}>
                     <View style={{ justifyContent: 'center', flex: 1 }}>
                         <Text
                             style={{
@@ -33,19 +46,30 @@ const HomeScreen = () => {
     const partners = useSelector((state) => state.partners);
 
     const featCampsite = campsites.campsitesArray.find((item) => item.featured);
-const featPromotion = promotions.promotionsArray.find(
-    (item) => item.featured
-);
-const featPartner = partners.partnersArray.find((item) => item.featured);
+    const featPromotion = promotions.promotionsArray.find(
+        (item) => item.featured
+    );
+    const featPartner = partners.partnersArray.find((item) => item.featured);
 
     return (
         <ScrollView>
-            <FeaturedItem item={featCampsite} />
-            <FeaturedItem item={featPromotion} />
-            <FeaturedItem item={featPartner} />
+            <FeaturedItem
+                item={featCampsite}
+                isLoading={campsites.isLoading}
+                errMess={campsites.errMess}
+            />
+            <FeaturedItem
+                item={featPromotion}
+                isLoading={promotions.isLoading}
+                errMess={promotions.errMess}
+            />
+            <FeaturedItem
+                item={featPartner}
+                isLoading={partners.isLoading}
+                errMess={partners.errMess}
+            />
         </ScrollView>
     );
 };
 
 export default HomeScreen;
-
